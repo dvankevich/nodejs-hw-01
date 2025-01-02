34a8ffaa8384442f147833a6cd0caf2ef04e747a
dvankevich <dvankevich@gmail.com>
2025-01-02 20:51:33 +0200

    get path via path.resolve

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,4 +1,7 @@
 import path from 'node:path';
 
 const pathToWorkDir = path.join(process.cwd());
-export const PATH_DB = path.join(pathToWorkDir, 'src', 'db','db.json');
+export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
+
+// https://nodejs.org/api/path.html#pathresolvepaths
+export const PATH_DB1 = path.resolve('src', 'db', 'db.json');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import { PATH_DB } from './constants/contacts.js';
+import { PATH_DB, PATH_DB1 } from './constants/contacts.js';
 
 console.log('PATH_DB: ', PATH_DB);
+console.log('PATH_DB1: ', PATH_DB1);
